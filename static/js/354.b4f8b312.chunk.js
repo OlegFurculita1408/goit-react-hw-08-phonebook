e8db145a0d691ee2e023ee96c4f8d9423a9d0f3c
OlegFurculita1408 <119543556+OlegFurculita1408@users.noreply.github.com>
2023-08-27 13:03:26 +0000

    Deploying to gh-pages from @ OlegFurculita1408/goit-react-hw-08-phonebook@420688521d9527cae43c556c01ce32747b7d437e 🚀

---
+++ static/js/354.b4f8b312.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[354],{7354:function(e,n,o){o.r(n),o.d(n,{default:function(){return h}});var t=o(8410),i=o(1087),r=o(184);function h(){return(0,r.jsxs)(t.W,{children:[(0,r.jsx)("h3",{children:"The page is not available"}),(0,r.jsx)(i.rU,{to:"/signUp",children:"Go to SignUp..."})]})}}}]);
+//# sourceMappingURL=354.b4f8b312.chunk.js.map
